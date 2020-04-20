297433605b8e74983dd4143c5cafe76cb1500370
Björn Meyer <bjoern.meyer@flagbit.de>
2020-04-20 08:22:34 +0200

    [SHOPWARE-7] moved from template to config

--- src/Resources/app/administration/src/module/sw-cms/elements/twitter-timeline/config/index.js
+++ src/Resources/app/administration/src/module/sw-cms/elements/twitter-timeline/config/index.js
@@ -9,6 +9,29 @@ Component.register('sw-cms-el-config-twitter-timeline', {
         Mixin.getByName('cms-element')
     ],
 
+    data() {
+        return {
+            selectOptions: [
+                {
+                    id: 'profile',
+                    name: this.$tc('sw-cms.elements.twitterTimeline.type.profile'),
+                },
+                {
+                    id: 'profileLikes',
+                    name: this.$tc('sw-cms.elements.twitterTimeline.type.profileLikes'),
+                },
+                {
+                    id: 'collections',
+                    name: this.$tc('sw-cms.elements.twitterTimeline.type.collections'),
+                },
+                {
+                    id: 'lists',
+                    name: this.$tc('sw-cms.elements.twitterTimeline.type.lists'),
+                }
+            ],
+        };
+    },
+
     created() {
         this.createdComponent();
     },
@@ -16,6 +39,14 @@ Component.register('sw-cms-el-config-twitter-timeline', {
     methods: {
         createdComponent() {
             this.initElementConfig('twitter-timeline');
+        },
+
+        showListsField() {
+            return (this.element.config.timelineType.value === 'lists');
+        },
+
+        showCollectionsField() {
+            return (this.element.config.timelineType.value === 'collections')
         }
     }
 });
